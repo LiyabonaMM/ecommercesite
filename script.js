0846d0d79c96c9adb43d102f453b964a0bf61970
Liyabona Mxhalisa <mxhalisalm@gmail.com>
2023-05-30 12:45:02 +0200

    4th Commit

--- script.js
+++ script.js
@@ -1,72 +1,118 @@
 // Array of products
 const products = [
-    {
-      name: "REBIRTH",
-      image: "https://i.postimg.cc/sXG84Vym/misunderstood.png",
-      price: "R400"
-    },
-    // Add more product objects here
-    {
-        name: "LUCID T-SHIRT",
-        image: "img/product2.png",
-        price: "R300"
-      },
-      {
-        name: "LUCID BENEFITS",
-        image: "img/product3.png",
-        price: "R650"
-      },
-      {
-        name: "LUCID HOODIE II",
-        image: "https://i.postimg.cc/ZKskrDzw/cranium.png",
-        price: "R600"
-      },
-      {
-        name: "LUCID INSOMNIA ",
-        image: "img/product5.png",
-        price: "R400"
-      },
-      {
-        name: "LUCID PANTS",
-        image: "https://i.postimg.cc/yYNLB13N/prdouc.png",
-        price: "R900"
-      },
-      {
-        name: "LUCID SKULL",
-        image: "https://i.postimg.cc/nh3kP6hD/fav.png",
-        price: "R700"
-      },
-  ];
+  {
+    name: "REBIRTH",
+    image: "https://i.postimg.cc/sXG84Vym/misunderstood.png",
+    price: "R400"
+  },
+
+  {
+    name: "LUCID T-SHIRT",
+    image: "img/product2.png",
+    price: "R300"
+  },
+  {
+    name: "LUCID BENEFITS",
+    image: "img/product3.png",
+    price: "R650"
+  },
+  {
+    name: "LUCID HOODIE II",
+    image: "https://i.postimg.cc/ZKskrDzw/cranium.png",
+    price: "R600"
+  },
+  {
+    name: "LUCID INSOMNIA",
+    image: "img/product5.png",
+    price: "R400"
+  },
+  {
+    name: "LUCID PANTS",
+    image: "https://i.postimg.cc/yYNLB13N/prdouc.png",
+    price: "R900"
+  },
+  {
+    name: "LUCID SKULL",
+    image: "https://i.postimg.cc/nh3kP6hD/fav.png",
+    price: "R700"
+  },
+];
+
+// Function to create a product element
+
+function createProductElement(product) {
+  // A container div for the product
+  const productDiv = document.createElement("div");
+  productDiv.className = "col-md-3";
+
+  // A div for the product image
+  const productImage = document.createElement("div");
+  productImage.className = "product-image";
+
+  // Creating  an img element and setting  its source and alt attributes
+  const img = document.createElement("img");
+  img.src = product.image;
+  img.alt = product.name;
+
+
+  // Append the img element to the product image div
+  productImage.appendChild(img);
+
+  // Creating  a div for the product details
+
+
+  const productDetails = document.createElement("div");
+  productDetails.className = "product-details";
+
+
+  // Creating  an h2 element for the product name and setting  its text content
+
+
+  const productName = document.createElement("h2");
+  productName.textContent = product.name;
+
+
+  // Creating  a p element for the product price and set its text content
+  const productPrice = document.createElement("p");
+  productPrice.textContent = "Price: " + product.price;
+
+  // Creating a button element for adding to cart
+
+
+  const buyButton = document.createElement("button");
+  buyButton.className = "buy-btn btn btn-primary";
+  buyButton.textContent = "Add to Cart";
+
+  // Appending  the product name, price, and button to the product details div
+
+
+  productDetails.appendChild(productName);
+  productDetails.appendChild(productPrice);
+  productDetails.appendChild(buyButton);
+
+  // Appending the product image and details divs to the product container div
+
+
+  productDiv.appendChild(productImage);
+  productDiv.appendChild(productDetails);
+
   
-  // Function to create a product element
-  function createProductElement(product) {
-    const productDiv = document.createElement("div");
-    productDiv.className = "col-md-3";
-  
-    const productHTML = `
-      <div class="product">
-        <div class="product-image">
-          <img src="${product.image}" alt="${product.name}">
-        </div>
-        <div class="product-details">
-          <h2>${product.name}</h2>
-          <p>Price: ${product.price}</p>
-          <button class="buy-btn btn btn-primary">Add to Cart</button>
-        </div>
-      </div>
-    `;
-  
-    productDiv.innerHTML = productHTML;
-  
-    return productDiv;
-  }
-  
-  // Get the container element to append the product elements
-  const productsContainer = document.querySelector("#products .row");
-  
-  // Loop through the products array and create product elements
-  products.forEach(product => {
-    const productElement = createProductElement(product);
-    productsContainer.appendChild(productElement);
-  });
-  
+  // Return the created product element
+  return productDiv;
+}
+
+// Getting  the container element to append the product elements
+
+
+const productsContainer = document.querySelector("#products .row");
+
+// Looping through the products array and create product elements
+
+
+for (let i = 0; i < products.length; i++) {
+  const productElement = createProductElement(products[i]);
+
+
+  // Appending the product element to the products container
+  productsContainer.appendChild(productElement);
+}
