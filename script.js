f68385a634476ff06fb735886c5a757fd68b3a70
Liyabona Mxhalisa <mxhalisalm@gmail.com>
2023-05-30 11:18:12 +0200

    third commit

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 // Array of products
 const products = [
     {
-      name: "LUCID HOODIE",
-      image: "img/product1.png",
-      price: "R600"
+      name: "REBIRTH",
+      image: "https://i.postimg.cc/sXG84Vym/misunderstood.png",
+      price: "R400"
     },
     // Add more product objects here
     {
-        name: "LUCID HOODIE",
-        image: "img/product1.png",
+        name: "LUCID T-SHIRT",
+        image: "img/product2.png",
+        price: "R300"
+      },
+      {
+        name: "LUCID BENEFITS",
+        image: "img/product3.png",
+        price: "R650"
+      },
+      {
+        name: "LUCID HOODIE II",
+        image: "https://i.postimg.cc/ZKskrDzw/cranium.png",
         price: "R600"
       },
       {
-        name: "LUCID HOODIE",
-        image: "img/product1.png",
-        price: "R600"
+        name: "LUCID INSOMNIA ",
+        image: "img/product5.png",
+        price: "R400"
       },
       {
-        name: "LUCID HOODIE",
-        image: "img/product1.png",
-        price: "R600"
+        name: "LUCID PANTS",
+        image: "https://i.postimg.cc/yYNLB13N/prdouc.png",
+        price: "R900"
       },
       {
-        name: "LUCID HOODIE",
-        image: "img/product1.png",
-        price: "R600"
-      },
-      {
-        name: "LUCID HOODIE",
-        image: "img/product1.png",
-        price: "R600"
-      },
-      {
-        name: "LUCID HOODIE",
-        image: "img/product1.png",
-        price: "R600"
+        name: "LUCID SKULL",
+        image: "https://i.postimg.cc/nh3kP6hD/fav.png",
+        price: "R700"
       },
   ];
   
